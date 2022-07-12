6a924cd63ff782e3a144b118b57e11903c8dee4d
skysaver00 <skysaver00@naver.com>
2022-07-12 16:56:09 +0900

    2022-07-12 Not Solved

--- 9093/9093.cpp
+++ 9093/9093.cpp
@@ -8,18 +8,38 @@ int main() {
     stack<char> maker;
 
     int t;
-    string str;
     cin >> t;
+    cin.ignore();
+    string str;
+
     for(int i = 0; i < t; i++) {
-        cin >> str;
+        getline(cin, str);
+        cout << "DFSFS: " << str << "\n";
         int len = str.length();
+        int flag = 0;
+        
         for(int j = 0; j < len; j++) {
-            maker.push(str[j]);
+            if(str[j] != ' ') maker.push(str[j]);
+
+            if(str[j] == ' ') {
+                while(!maker.empty()) {
+                    if(flag != 0) {
+                        cout << " ";
+                    }
+                    flag++;
+                    cout << maker.top();
+                    maker.pop();
+                }
+            } else if(j == len - 1) {
+                cout << " ";
+                while(!maker.empty()) {
+                    cout << maker.top();
+                    maker.pop();
+                }
+            }
         }
-        while(!maker.empty()) {
-            cout << maker.top();
-            maker.pop();
-        }
+        cin.ignore();
+        flag = 0;
     }
     return 0;
 }
