29fc396f6d098d3fffaa19c8d277e920b82a0608
skysaver00 <skysaver00@naver.com>
2022-07-13 22:46:14 +0900

    2021-07-13 Not Solved

--- 9093/9093.cpp
+++ 9093/9093.cpp
@@ -1,45 +1,20 @@
 #include <iostream>
-#include <string>
 #include <stack>
+#include <string>
 
 using namespace std;
 
 int main() {
-    stack<char> maker;
+    string sentence;
+    stack<char> stack;
 
     int t;
     cin >> t;
     cin.ignore();
-    string str;
-
     for(int i = 0; i < t; i++) {
-        getline(cin, str);
-        cout << "DFSFS: " << str << "\n";
-        int len = str.length();
-        int flag = 0;
-        
-        for(int j = 0; j < len; j++) {
-            if(str[j] != ' ') maker.push(str[j]);
+        cin.getline(sentence);
 
-            if(str[j] == ' ') {
-                while(!maker.empty()) {
-                    if(flag != 0) {
-                        cout << " ";
-                    }
-                    flag++;
-                    cout << maker.top();
-                    maker.pop();
-                }
-            } else if(j == len - 1) {
-                cout << " ";
-                while(!maker.empty()) {
-                    cout << maker.top();
-                    maker.pop();
-                }
-            }
-        }
-        cin.ignore();
-        flag = 0;
     }
+
     return 0;
 }
