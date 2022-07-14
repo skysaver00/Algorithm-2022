f3f1b2f4985e93175dae7be09a3ebd812daa6625
skysaver00 <skysaver00@naver.com>
2022-07-14 23:33:51 +0900

    Not Solved 2022-07-14

---
+++ 1920/1920.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100001];
+
+bool binary() {
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+
+    int m;
+    cin >> m;
+    
+    for(int i = 0; i < m; i++) {
+        int 
+    }
+
+    return 0;
+}
