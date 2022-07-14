b38a80185682a9c49640d26962bb3222d4d42dc7
skysaver00 <skysaver00@naver.com>
2022-07-14 22:46:09 +0900

    Not Solved 2022-07-14

--- 2609/2609.cpp
+++ 2609/2609.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int getgcd(int val1, int val2) {
+    if(val1 % val2 == 0) return val2;
+    else {
+        getgcd(val1, val1 % val2);
+    }
+}
+
 int main() {
     int val1, val2;
     cin >> val1 >> val2;
@@ -14,8 +21,7 @@ int main() {
         val2 = temp;
     }
 
-    if(val1 % val2 == 0) gcd = val2;
-    else {
-        
-    }
+    gcd = getgcd(val1, val2);
+    cout << gcd;
+    return 0;
 }
