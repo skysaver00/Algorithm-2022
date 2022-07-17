194c672a4405030c789cacf0478d22c4c2637406
skysaver00 <skysaver00@naver.com>
2022-07-17 23:06:18 +0900

    Not Solved 2022-07-17

---
+++ 1934/1934.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int getgcd(int val1, int val2) {
+    
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+
+    }
+
+    return 0;
+}
