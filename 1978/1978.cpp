0ca8373b03c4b5cd38ef810f7c675b010520053f
skysaver00 <skysaver00@naver.com>
2022-07-17 23:14:37 +0900

    Not Solved 2022-07-17

---
+++ 1978/1978.cpp
@@ -0,0 +1,20 @@
+//O(N)
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+
+        if(val < 2) {
+            
+        }
+    }
+}
