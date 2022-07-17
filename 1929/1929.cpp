a492f34c54548d470596aaf568666a4b04b05dbf
skysaver00 <skysaver00@naver.com>
2022-07-17 23:50:40 +0900

    Solved Silver III 2022-07-17

--- 1929/1929.cpp
+++ 1929/1929.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 bool isPrime[1000001];
 
-int mian() {
+int main() {
     int m, n;
     cin >> m >> n;
 
     for(int i = 2; i <= n; i++) {
         if(isPrime[i]) continue;
         else {
-            for(int j = i * i; j <= n; j += i) {
+            for(long long j = (long long) i * i; j <= n; j += i) {
                 isPrime[j] = true;
             }
         }
     }
+    isPrime[1] = true;
 
     for(int i = m; i <= n; i++) if(!isPrime[i]) cout << i << "\n";
     return 0;
