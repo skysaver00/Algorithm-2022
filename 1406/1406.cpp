722964c8fda0199a818116eaaf8e445cb4ffd1b8
skysaver00 <skysaver00@naver.com>
2022-07-25 19:08:46 +0900

    Not Solved 2022-07-25

---
+++ 1406/1406.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    stack<char> left;
+    stack<char> right;
+
+    string word;
+    cin >> word;
+    int len = word.length();
+
+    for(int i = 0; i < len; i++) {
+        left.push(word[i]);
+    }
+
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        char one, two;
+        cin >> one;
+        if(one == 'P') cin >> two;
+
+        if(one == 'L') {
+            if(left.empty()) continue;
+            else {
+                right.push(left.top());
+                left.pop();
+            }
+        }
+        else if(one == 'D') {
+            if(right.empty()) continue;
+            else {
+                left.push(right.top());
+                right.pop();
+            }
+        }
+        else if(one == 'B') {
+            if(left.empty()) continue;
+            else left.pop();
+        }
+        else {
+            left.push(two);
+        }
+
+        cout << left.top() << " " << right.top() << "\n";
+    }
+
+    while(!left.empty()) {
+        right.push(left.top());
+        left.pop();
+    }
+
+    while(!right.empty()) {
+        cout << right.top();
+        right.pop();
+    }
+
+    return 0;
+}
