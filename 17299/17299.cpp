b94a460a37c629b0f800b3a6468a187f2d6189ff
skysaver00 <skysaver00@naver.com>
2022-08-04 01:31:38 +0900

    Solved Gold III 2022-08-04

---
+++ 17299/17299.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int arr[1000001];
+int num[1000001];
+int ans[1000001];
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < n; i++) num[arr[i]]++;
+
+    stack <int> stk;
+    stk.push(0);
+    for(int i = 1; i < n; i++) {
+        if(stk.empty()) stk.push(i);
+
+        while(!stk.empty() && num[arr[stk.top()]] < num[arr[i]]) {
+            ans[stk.top()] = arr[i];
+            stk.pop();
+        }
+        stk.push(i);
+    }
+    
+    while(!stk.empty()) {
+        ans[stk.top()] = -1;
+        stk.pop();
+    }
+
+    for(int i = 0; i < n; i++) cout << ans[i] << ' ';
+    cout << '\n';
+
+    return 0;
+}
