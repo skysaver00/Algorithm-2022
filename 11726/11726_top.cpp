d7fff6dee3fefd05c83820b6b14bdc668346a82d
skysaver00 <skysaver00@naver.com>
2022-08-07 20:53:28 +0900

    Solved Silver III Top 2022-08-07

---
+++ 11726/11726_top.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int ans[1001];
+
+int dp(int x) {
+    if(x == 1) return 1;
+    if(x == 2) return 2;
+    if(ans[x] > 0) return ans[x];
+    ans[x] = (dp(x - 1) + dp(x - 2)) % 10007;
+
+    return ans[x];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << dp(n);
+
+    return 0;
+}
