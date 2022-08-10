4e1ed896e1e028689381f3af56951701d626efd7
skysaver00 <skysaver00@naver.com>
2022-08-11 00:00:40 +0900

    Solved Silver II Top 2022-08-11

--- 1699/1699_top.cpp
+++ 1699/1699_top.cpp
@@ -5,13 +5,19 @@ int ans[100001];
 
 int dp(int x) {
     if(ans[x] > 0) return ans[x];
-    int temp = dp()
+    for(int i = 1; i * i <= x; i++) {
+        int temp = dp(x - i * i) + 1;
+        if(ans[x] == 0) ans[x] = temp;
+        if(temp < ans[x]) ans[x] = temp;
+    }
+    return ans[x];
 }
 
 int main() {
     int n;
     cin >> n;
 
-    for(int i = 1; i * i <= n; i++) ans[i] = 1;
+    for(int i = 1; i * i <= n; i++) ans[i * i] = 1;
     for(int i = 1; i <= n; i++) dp(i);
+    cout << dp(n);
 }
