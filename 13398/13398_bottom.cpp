99b3dc25607268393a242e702c288a951b35374d
skysaver00 <skysaver00@naver.com>
2022-08-15 15:25:58 +0900

    Solved Gold V Bottom 2022-08-15

--- 13398/13398_bottom.cpp
+++ 13398/13398_bottom.cpp
@@ -14,8 +14,6 @@ int main() {
     lef[0] = arr[0];
     for(int i = 1; i < n; i++) lef[i] = max(arr[i], lef[i - 1] + arr[i]);
 
-
-
     rig[n - 1] = arr[n - 1];
     for(int i = n - 2; i >= 0; i--) rig[i] = max(arr[i], rig[i + 1] + arr[i]);
 
