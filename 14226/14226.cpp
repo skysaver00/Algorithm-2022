db8a46efb580dc097a7e4c8e00feeb57bfa0c657
skysaver00 <skysaver00@naver.com>
2022-08-15 19:06:56 +0900

    Solved Gold IV 2022-08-15

--- 14226/14226.cpp
+++ 14226/14226.cpp
@@ -1,50 +1,41 @@
 #include <iostream>
 #include <queue>
-#include <tuple>
 
 using namespace std;
-bool visit[1001][1001];
+int visit[2000][2000];
 
 int main() {
     int n;
-    queue<tuple<int, int, int>> que;
     cin >> n;
-
-    que.push(make_tuple(1, 0, 0));
+    queue <pair<int, pair<int, int>>> que;
+    que.push(make_pair(1, make_pair(0, 0)));
     while(1) {
-        int distance = get<0>(que.front());
-        int clipboard = get<1>(que.front());
-        int time = get<2>(que.front());
+        int distance = que.front().first;
+        int clipboard = que.front().second.first;
+        int time = que.front().second.second;
 
-        if(get<0>(que.front()) == n) {
-            cout << get<2>(que.front());
+        if(distance == n) {
+            cout << time;
             return 0;
         }
 
         if(distance > 0 && distance < 2000) {
             if(visit[distance][distance] == false) {
+                que.push(make_pair(distance, make_pair(distance, time + 1)));
                 visit[distance][distance] = true;
-                que.push(make_tuple(distance, distance, time + 1));
             }
             if(visit[distance - 1][clipboard] == false) {
-                visit[distance - 1][clipboard] == true;
-                que.push(make_tuple(distance - 1, clipboard, time + 1));
+                que.push(make_pair(distance - 1, make_pair(clipboard, time + 1)));
+                visit[distance - 1][clipboard] = true;
             }
         }
+        
         if(clipboard > 0 && distance + clipboard < 2000) {
             if(visit[distance + clipboard][clipboard] == false) {
-                visit[distance + clipboard][clipboard] == true;
-                que.push(make_tuple(distance + clipboard, clipboard, time + 1));
+                que.push(make_pair(distance + clipboard, make_pair(clipboard, time + 1)));
+                visit[distance + clipboard][clipboard] = true;
             }
         }
-        que.pop();
-
-
-        cout << get<0>(que.front()) << ' ' << get<1>(que.front()) << ' ' << get<2>(que.front()) << "\n";
-        que.push(make_tuple(get<0>(que.front()), get<0>(que.front()), get<2>(que.front()) + 1));
-        que.push(make_tuple(get<0>(que.front()) + get<1>(que.front()), get<0>(que.front()), get<2>(que.front()) + 1));
-        if(get<0>(que.front()) > 1) que.push(make_tuple(get<0>(que.front()) - 1, get<0>(que.front()), get<2>(que.front()) + 1));
-
         que.pop();
     }
 }
