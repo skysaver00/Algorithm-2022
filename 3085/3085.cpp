bcc154c6a9c8bd330afd29312700faa2278baa15
skysaver00 <skysaver00@naver.com>
2022-08-17 17:58:22 +0900

    Solved Silver III 2022-08-17

--- 3085/3085.cpp
+++ 3085/3085.cpp
@@ -2,11 +2,10 @@
 #include <vector>
 
 using namespace std;
-vector <string> real(51);
 vector <string> copi(51);
+int n;
 
 int main() {
-    int n;
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> copi[i];
@@ -15,9 +14,8 @@ int main() {
     int max = 1;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
+            if(i > 0) swap(copi[i - 1][j], copi[i][j]);
             int now = 1;
-            if(i > 0) swap(copi[i - 1][j], copi[i][j]);
-            now = 1;
             for(int k = 1; k < n; k++) {
                 if(copi[i][k - 1] == copi[i][k]) now++;
                 else {
@@ -58,7 +56,7 @@ int main() {
             if(max < now) max = now;
             if(j > 0) swap(copi[i][j - 1], copi[i][j]);
 
-            if(i > 0) swap(copi[i + 1][j], copi[i][j]);
+            if(i < n) swap(copi[i + 1][j], copi[i][j]);
             now = 1;
             for(int k = 1; k < n; k++) {
                 if(copi[i][k - 1] == copi[i][k]) now++;
@@ -77,9 +75,9 @@ int main() {
                 }
             }
             if(max < now) max = now;
-            if(i > 0) swap(copi[i + 1][j], copi[i][j]);
-            
-            if(j > 0) swap(copi[i][j + 1], copi[i][j]);
+            if(i < n) swap(copi[i + 1][j], copi[i][j]);
+
+            if(j < n) swap(copi[i][j + 1], copi[i][j]);
             now = 1;
             for(int k = 1; k < n; k++) {
                 if(copi[i][k - 1] == copi[i][k]) now++;
@@ -91,7 +89,6 @@ int main() {
             if(max < now) max = now;
             now = 1;
             for(int k = 1; k < n; k++) {
-                now = 1;
                 if(copi[k - 1][j] == copi[k][j]) now++;
                 else {
                     if(max < now) max = now;
@@ -99,7 +96,7 @@ int main() {
                 }
             }
             if(max < now) max = now;
-            if(j > 0) swap(copi[i][j + 1], copi[i][j]);
+            if(j < n) swap(copi[i][j + 1], copi[i][j]);
         }
     }
     cout << max;
