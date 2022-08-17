517ba29510a1b1005d39eaeb96873d0c497f73ec
skysaver00 <skysaver00@naver.com>
2022-08-17 15:31:44 +0900

    Solved Gold V 2022-08-17

--- 5014/5014.cpp
+++ 5014/5014.cpp
@@ -8,28 +8,10 @@ bool visited[1000001];
 int main() {
     int F, S, G, U, D;
     cin >> F >> S >> G >> U >> D;
-    if((S - G) % 2 == 1) {
-        if(U % 2 == 0 && D % 2 == 0) {
-            cout << "use the stairs";
-            return 0;
-        }
-    }
-    if(S > G && D == 0) {
-        cout << "use the stairs";
-        return 0;
-    }
-    if(S < G && U == 0) {
-        cout << "use the stairs";
-        return 0;
-    }
-    if(S + U > F && S - D <= 0) {
-        cout << "use the stairs";
-        return 0;
-    }
 
     queue <pair<int, int>> que;
     que.push(make_pair(S, 0));
-    while(1) {
+    while(!que.empty()) {
         int now = que.front().first;
         int time = que.front().second;
         if(now == G) {
@@ -40,7 +22,7 @@ int main() {
             visited[now + U] = true;
             que.push(make_pair(now + U, time + 1));
         }
-        if(now - D >= 0 && visited[now - D] == false) {
+        if(now - D > 0 && visited[now - D] == false) {
             visited[now - D] = true;
             que.push(make_pair(now - D, time + 1));
         }
