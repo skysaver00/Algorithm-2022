39c820778a9ae359a85b90ba66900f975f6c30e4
skysaver00 <skysaver00@naver.com>
2022-08-20 23:22:09 +0900

    Solved Silver I 2022-08-20

--- 6064/6064.cpp
+++ 6064/6064.cpp
@@ -2,34 +2,50 @@
 
 using namespace std;
 int m, n, x, y;
+bool arr[40002];
 
 int main() {
     int t;
     cin >> t;
     for(int i = 0; i < t; i++) {
+        fill_n(arr, 40001, 0);
         cin >> m >> n >> x >> y;
         int xx, yy;
-        xx = yy = 1;
-        
-        if(m % 2 == 0 && n % 2 == 0 && abs(x - y) % 2 == 1) {
-            cout << -1 << "\n";
-            continue;
+        int ans;
+        bool flag = false;
+        bool flag2 = false;
+        if(m > n) {
+            xx = yy = ans = y;
+            flag = false;
+        } else {
+            xx = yy = ans = x;
+            flag = true;
         }
 
-        int upcount = m - n;
-        int ans = 0;
-        while(xx != x) {
-            xx++;
-            yy++;
-            if(xx > m) xx = 1;
-            if(yy > n) yy = 1;
-            ans++;
+        if(flag == false) {
+            while(xx != x) {
+                xx = (xx + n) % m;
+                if(xx == 0) xx = m;
+                if(arr[xx] == false) arr[xx] = true;
+                else {
+                    flag2 = true;
+                    break;
+                }
+                ans += n;
+            }
+        } else {
+            while(yy != y) {
+                yy = (yy + m) % n;
+                if(yy == 0) yy = n;
+                if(arr[yy] == false) arr[yy] = true;
+                else {
+                    flag2 = true;
+                    break;
+                }
+                ans += m;
+            }
         }
-        while(yy != y) {
-            if(upcount + yy < 1) yy = n + upcount + yy;
-            else yy = (upcount + yy) % n;
-            ans += m;
-        }
-        cout << ans << "\n";
+        if(!flag2) cout << ans << "\n";
+        else cout << "-1\n";
     }
 }
