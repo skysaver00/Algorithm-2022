87b93c63de9486c7d80c2b0257d45ee1570ca57d
skysaver00 <skysaver00@naver.com>
2022-08-20 19:10:01 +0900

    Not Solved 2022-08-20

---
+++ 14500/14500.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int arr[501][501];
+int n, m;
+
+int T[4][4] = {
+    
+}
+int O[4];
+int I[4][2];
+int S1[4][2];
+int S2[4][2];
+int L1[4][4];
+int L2[4][4];
+
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+}
