55250ade93aa8487e0a51ba352325f33555beb5d
skysaver00 <skysaver00@naver.com>
2022-08-22 04:14:10 +0900

    Not Solved 2022-08-22

---
+++ 15665/15665.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int arr[10];
+int brr[10];
+
+void go(int index, int n, int m) {
+    if(index == m) {
+        for(int i = 0; i < m; i++) cout << brr[arr[i]] << ' ';
+        cout << "\n";
+        return;
+    }
+
+    for(int i = index; i < n; i++) {
+        arr[index] = i;
+        go(index + 1, n, m);
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> brr[i];
+    sort(brr, brr + n);
+    go(0, n, m);
+}
