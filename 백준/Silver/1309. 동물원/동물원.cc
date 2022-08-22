50bfee5ab50977e3eebdc77980c21f0e409ec275
Laniakea <skysaver00@naver.com>
2022-08-23 03:21:39 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,18 +1,16 @@
 #include <iostream>
 
 using namespace std;
-int ans[100001][3];
+int arr[100001][3];
 
 int main() {
     int n;
     cin >> n;
-    ans[1][0] = 1;
-    ans[1][1] = 1;
-    ans[1][2] = 1;
-    for(int i = 2; i <= n; i++) {
-        ans[i][0] = (ans[i - 1][0] + ans[i - 1][1] + ans[i - 1][2]) % 9901;
-        ans[i][1] = (ans[i - 1][0] + ans[i - 1][2]) % 9901;
-        ans[i][2] = (ans[i - 1][0] + ans[i - 1][1]) % 9901;
+    arr[0][0] = arr[0][1] = arr[0][2] = 1;
+    for(int i = 1; i < n; i++) {
+        arr[i][0] = (arr[i - 1][1] + arr[i - 1][2]) % 9901;
+        arr[i][1] = (arr[i - 1][0] + arr[i - 1][2]) % 9901;
+        arr[i][2] = (arr[i - 1][0] + arr[i - 1][1] + arr[i - 1][2]) % 9901;
     }
-    cout << (ans[n][0] + ans[n][1] + ans[n][2]) % 9901;
+    cout << (arr[n - 1][0] + arr[n - 1][1] + arr[n - 1][2]) % 9901;
 }
