a22fc1874d760fb6e59a368235a0a30c827aa298
Laniakea <skysaver00@naver.com>
2022-08-22 14:50:50 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int arr[10];
+int brr[10];
+
+void go(int index, int start, int n, int m) {
+    if(index == m) {
+        for(int i = 0; i < m; i++) cout << brr[arr[i]] << ' ';
+        cout << "\n";
+        return;
+    }
+
+    for(int i = start; i < n; i++) {
+        arr[index] = i;
+        go(index + 1, i, n, m);
+    }
+}
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int temp[10];
+    for(int i = 0; i < n; i++) cin >> temp[i];
+    sort(temp, temp + n);
+
+    int now = 0;
+    brr[now] = temp[now];
+    for(int i = 1; i < n; i++) {
+        if(brr[now] == temp[i]) continue;
+        else {
+            now++;
+            brr[now] = temp[i];
+        }
+    }
+
+    go(0, 0, now + 1, m);
+}
