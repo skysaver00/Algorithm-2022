8b89bc81ccc679bae16487417e4b13d042f65bf6
skysaver00 <skysaver00@naver.com>
2022-08-23 02:22:21 +0900

    Solved Silver III 2022-08-23

--- 9095/9095_permutation.cpp
+++ 9095/9095_permutation.cpp
@@ -6,7 +6,7 @@ int go(int sum, int goal) {
     if(sum > goal) return 0;
     if(sum == goal) return 1;
     int now = 0;
-    for(int i = 1; i <= 3; i++) now += go(sum + 1, goal);
+    for(int i = 1; i <= 3; i++) now += go(sum + i, goal);
     return now;
 }
 
