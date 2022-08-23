93e837cf74ea318be6a58852fea450f58b0da1d3
skysaver00 <skysaver00@naver.com>
2022-08-23 21:12:15 +0900

    Solved Silver I 2022-08-23

--- 11057/11057_bottom.cpp
+++ 11057/11057_bottom.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int ans[1001][10];
+int arr[1000][10];
 
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i <= 9; i++) ans[1][i] = 1;
-    for(int i = 2; i <= n; i++) {
-        for(int j = 0; j <= 9; j++) {
+    for(int i = 0; i < 10; i++) arr[0][i] = 1;
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < 10; j++) {
             for(int k = 0; k <= j; k++) {
-                ans[i][j] += ans[i - 1][k];
-                ans[i][j] %= 10007;
+                arr[i][j] = (arr[i][j] + arr[i - 1][k]) % 10007;
             }
         }
     }
-    int val = 0;
-    for(int i = 0; i <= 9; i++) {
-        val += ans[n][i];
-        val %= 10007;
+    int ans = 0;
+    for(int i = 0; i < 10; i++) {
+        ans = ans + arr[n - 1][i];
+        ans %= 10007;
     }
-    cout << val;
+    cout << ans;
 }
