af4953498db3bc56360ff2f9e5711a7e7e77f692
skysaver00 <skysaver00@naver.com>
2022-08-24 23:16:26 +0900

    Solved Silver V 2022-08-24

--- 11723/11723.cpp
+++ 11723/11723.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int s;
 
 int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
     int m, n;
     cin >> m;
     for(int i = 0; i < m; i++) {
@@ -12,19 +16,19 @@ int main() {
         cin >> str;
         if(str.compare("add") == 0) {
             cin >> n; n--;
-            s = s | (1 << n);
+            s = (s | (1 << n));
         } else if(str.compare("remove") == 0) {
             cin >> n; n--;
-            s = s & ~(1 << n);
+            s = (s & ~(1 << n));
         } else if(str.compare("check") == 0) {
             cin >> n; n--;
-            if(s & (1 << n) == 1) cout << "1\n";
+            if((s & (1 << n))) cout << "1\n";
             else cout << "0\n";
         } else if(str.compare("toggle") == 0) {
             cin >> n; n--;
-            s = s ^ (1 << n);
+            s = (s ^ (1 << n));
         } else if(str.compare("all") == 0) {
-            s = (1 << n) - 1;
+            s = ((1 << 20) - 1);
         } else {
             s = 0;
         }
