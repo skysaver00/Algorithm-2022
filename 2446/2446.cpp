22b19ad3eaf29ebae4eef74000c989b5643d781b
skysaver00 <skysaver00@naver.com>
2022-08-25 23:21:16 +0900

    Solved Bronze III 2022-08-25

--- 2446/2446.cpp
+++ 2446/2446.cpp
@@ -6,13 +6,13 @@ int main() {
     int n;
     cin >> n;
     for(int i = 2 * n - 1; i >= 1; i -= 2) {
-        for(int j = 2 * n - i; j > 0; j--) cout << ' ';
+        for(int j = 2 * n - 1 - i; j > 0; j -= 2) cout << ' ';
         for(int j = i; j > 0; j--) cout << '*';
         cout << "\n";
     }
 
-    for(int i = 2; i <= 2 * n - 1; i += 2) {
-        for(int j = n - i; j > 0; j--) cout << ' ';
+    for(int i = 3; i <= 2 * n - 1; i += 2) {
+        for(int j = 2 * n - 1 - i; j > 0; j -= 2) cout << ' ';
         for(int j = 0; j < i; j++) cout << '*';
         cout << "\n";
     }
