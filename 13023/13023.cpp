6d5ba9e19a71d8c37d7930fde15cee7c30c08149
skysaver00 <skysaver00@naver.com>
2022-08-25 21:46:51 +0900

    Solved Gold V 2022-08-25

--- 13023/13023.cpp
+++ 13023/13023.cpp
@@ -9,16 +9,13 @@ bool flag = false;
 void dfs(int x, int index) {
     if(flag == true) return;
     if(index == 4) {
-        int flag = true;
+        flag = true;
         return;
     }
-    cout << x << ' ' << index << ' ' << vec[x].size() << "\n";
-
 
     check[x] = true;
     int len = vec[x].size();
     for(int i = 0; i < len; i++) {
-        cout << vec[x][i] << '\n';
         if(!check[vec[x][i]]) dfs(vec[x][i], index + 1);
     }
     check[x] = false;
