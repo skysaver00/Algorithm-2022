d7f8a61796d938e3a9e462581999f93c09446575
Laniakea <skysaver00@naver.com>
2022-08-26 13:26:49 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 312 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+int ans[14];
+int arr[14];
+int brr[14];
+int minimum = 1000000001;
+int maximum = -1000000001;
+int check[14];
+
+void go(int index, int n) {
+    if(index == n) {
+        int answer = ans[0];
+        for(int i = 0; i < n; i++) {
+            if(arr[brr[i]] == 0) answer += ans[i + 1];
+            if(arr[brr[i]] == 1) answer -= ans[i + 1];
+            if(arr[brr[i]] == 2) answer *= ans[i + 1];
+            if(arr[brr[i]] == 3) answer /= ans[i + 1];
+        }
+        if(minimum > answer) minimum = answer;
+        if(maximum < answer) maximum = answer;
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(check[i]) continue;
+        check[i] = true;
+        brr[index] = i;
+        go(index + 1, n);
+        check[i] = false;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> ans[i];
+
+    int now = 0;
+    for(int i = 0; i < 4; i++) {
+        int val;
+        cin >> val;
+        for(int j = 0; j < val; j++) {
+            arr[now] = i;
+            now++;
+        }
+    }
+
+    go(0, n - 1);
+    cout << maximum << '\n';
+    cout << minimum;
+}
