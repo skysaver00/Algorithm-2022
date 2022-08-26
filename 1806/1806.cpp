56b2e6c0205aee32744875f336febb032238bf9c
skysaver00 <skysaver00@naver.com>
2022-08-26 15:55:22 +0900

    Not Solved 2022-08-26

--- 1806/1806.cpp
+++ 1806/1806.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 
 using namespace std;
-int arr[100002];
+int arr[100005];
+int lef, rig;
+int value;
+int include = 100001;
 
 int main() {
     int n, s;
     cin >> n >> s;
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    
+    value = arr[lef];
+    if(value >= s) include = rig - lef + 1;
+    cout << value << ' ' << lef << ' ' << rig << '\n';
+
+    while(rig <= n) {
+        if(value >= s) if(rig - lef + 1 < include) include = rig - lef + 1;
+        if(value < s) {
+            rig++;
+            value += arr[rig];
+        } else {
+            value -= arr[lef];
+            lef++;
+        }
+        cout << include << ' ' << value << ' ' << lef << ' ' << rig << '\n';
+    }
+    if(include == 100001) cout << 0;
+    else cout << include;
 }
