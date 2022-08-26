fc1d0ea8400d170af111af055fa669568d94196e
skysaver00 <skysaver00@naver.com>
2022-08-26 16:24:23 +0900

    Not Solved 2022-08-26

--- 15683/15683.cpp
+++ 15683/15683.cpp
@@ -5,6 +5,23 @@ using namespace std;
 int room[10][10];
 int watched[10][10];
 vector <pair<int, int>> cameraloc[10];
+int cameratype[10];
+int looktype[10];
+int val;
+
+void go(int index, int n, int m) {
+    if(index == m) {
+        val++;
+        for(int i = 0; i < m; i++) cout << looktype[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        looktype[index] = i;
+        go(index + 1, n, m);
+    }
+}
 
 int main() {
     int n, m;
@@ -14,9 +31,15 @@ int main() {
         for(int j = 0; j < m; j++) {
             cin >> room[i][j];
             watched[i][j] = room[i][j];
-            if(room[i][j] != 0 || room[i][j] != 6) camera++;
+            if(room[i][j] != 0 && room[i][j] != 6) {
+                cameratype[camera] = room[i][j];
+                cameraloc[i].push_back(make_pair(i, j));
+                camera++;
+            }
         }
     }
+    cout << camera << "\n";
 
-
+    go(0, 4, camera);
+    cout << val;
 }
