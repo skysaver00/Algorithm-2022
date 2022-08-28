9ae873db0fae8cf298c8a1ef5798741ecdc0f9b4
skysaver00 <skysaver00@naver.com>
2022-08-28 14:59:35 +0900

    Solved Silver I 2022-08-28

--- 2178/2178_breadth.cpp
+++ 2178/2178_breadth.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int n, m;
+string straxis[101];
 int axis[101][101];
 int check[101][101];
 queue <pair<int, int>> que;
@@ -12,28 +13,41 @@ queue <pair<int, int>> que;
 int addx[4] = {1, 0, -1, 0};
 int addy[4] = {0, 1, 0, -1};
 
-int startbfs(int x, int y) {
-    if(axis[x][y] == 0) return;
-    else {
-        check[x][y] = 1;
-        que.push(make_pair(x, y));
-    }
+void startbfs(int x, int y) {
+    check[x][y] = 1;
+    que.push(make_pair(x, y));
 
     int val;
     while(!que.empty()) {
         int topx = que.front().first;
         int topy = que.front().second;
         val = check[topx][topy];
+        
+        for(int i = 0; i < 4; i++) {
+            int newx, newy;
+            newx = topx + addx[i];
+            newy = topy + addy[i];
+
+            if(newx >= 0 && newx < n && newy >= 0 && newy < m);
+            else continue;
+            if(check[newx][newy] > 0) continue;
+            if(axis[newx][newy] == 0) continue;
+
+            check[newx][newy] = val + 1;
+            que.push(make_pair(newx, newy));
+        }
+        que.pop();
     }
+    return;
 }
 
 int main() {
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) cin >> axis[i][j];
+        cin >> straxis[i];
+        for(int j = 0; j < m; j++) axis[i][j] = straxis[i][j] - 0x30;
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) startbfs(i, j);
-    }
+    startbfs(0, 0);
+    cout <<  check[n - 1][m - 1];
 }
