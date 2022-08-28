7e9691b289e91d911256f10686045a27768226e6
skysaver00 <skysaver00@naver.com>
2022-08-28 15:02:57 +0900

    Not Solved 2022-08-28

---
+++ 7576/7576_breadth.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int axis[1001][1001];
+int check[1001][1001];
+queue <int> que;
+
+int addx[4] = {1, 0, -1, 0};
+int addy[4] = {0, 1, 0, -1};
+
+void startbfs()
+
+int main() {
+    cin >> n >> m;
+
+}
