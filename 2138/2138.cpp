9ece5d2b414962e3ae394deac39a45038d249ef2
skysaver00 <skysaver00@naver.com>
2022-09-01 23:10:36 +0900

    Not Solved 2022-09-01

---
+++ 2138/2138.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+string str;
+bool arr[100001];
+int n;
+
+int main() {
+    cin >> n;
+    cin >> str;
+    for(int i = 0; i < n; i++) arr[i] = str[i] - 0x30;
+
+    int ans1, ans2;
+    ans1 = ans2 = 0;
+    
+}
