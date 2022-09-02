25b0d77a646502839d876a7b0389fe42db796d9b
skysaver00 <skysaver00@naver.com>
2022-09-03 01:00:48 +0900

    Not Solved 2022-09-03

---
+++ 1285/1285.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+char arr[21][21];
+char arr2[21][21];
+char copy[21][21];
+
+int main() {
+    int n;
+    for(int i = 0; i < n; i++) cin >> copy[i];
+}
