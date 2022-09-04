32bd700200e7edd9ac00812dd4d33471e920b18b
skysaver00 <skysaver00@naver.com>
2022-09-04 21:30:57 +0900

    Not Solved 2022-09-04

---
+++ 9935/9935.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+string str, str2;
+stack <pair<char, int>> stk;
+stack <char> stk2;
+
+int main() {
+    cin >> str;
+    int len = str.length();
+
+    cin >> str2;
+    for(int i = 0; i < len; i++) {
+        if(str[i] == )
+    }
+}
