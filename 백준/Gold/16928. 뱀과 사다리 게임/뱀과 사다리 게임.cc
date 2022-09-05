6e3a50c9fbda828de10561022c135d349ed4979a
Laniakea <skysaver00@naver.com>
2022-09-05 20:00:26 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+int check[102];
+int n, m;
+vector < pair < int, int >> ladde;
+vector < pair < int, int >> snake;
+queue < int > que;
+
+void startbfs(int start) {
+    check[start] = 1;
+    que.push(start);
+
+    while (!que.empty()) {
+        if (check[100] > 0) {
+            while (!que.empty()) que.pop();
+            return;
+        }
+
+        int top = que.front();
+        for (int i = 1; i <= 6; i++) {
+            int next = top + i;
+            if (check[next] > 0) continue;
+            check[next] = check[top] + 1;
+            bool flag = false;
+
+            for (int i = 0; i < n; i++) {
+                int val = ladde[i].first;
+                int val2 = ladde[i].second;
+                if (val == next) {
+                    flag = true;
+                    if (check[val2] > 0) {
+                        if (check[val2] > check[next]) {
+                            que.push(val2);
+                            check[val2] = check[next];
+                        }
+                    } else {
+                        que.push(val2);
+                        check[val2] = check[next];
+                    }
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                int val = snake[i].first;
+                int val2 = snake[i].second;
+                if (val == next) {
+                    flag = true;
+                    if (check[val2] > 0) {
+                        if (check[val2] > check[next]) {
+                            que.push(val2);
+                            check[val2] = check[next];
+                        }
+                    } else {
+                        que.push(val2);
+                        check[val2] = check[next];
+                    }
+                }
+            }
+
+            if(flag) {}
+            else {
+                que.push(next);
+            }
+        }
+        que.pop();
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        ladde.push_back(make_pair(x, y));
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        snake.push_back(make_pair(u, v));
+    }
+
+    startbfs(1);
+    cout << check[100] - 1;
+    return 0;
+}
