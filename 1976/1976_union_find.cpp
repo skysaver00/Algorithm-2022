1bb5a293cb6fb6123fbf3d76b82c7f26fb5e3b08
skysaver00 <skysaver00@naver.com>
2022-09-05 23:33:02 +0900

    Not Solved 2022-09-05

---
+++ 1976/1976_union_find.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+int arr[201];
+bool arr2[201][201];
+int arr3[1001];
+
+int find(int x) {
+    if(arr[x] == x) return x;
+    return arr[x] = find(arr[x]);
+}
+
+void merge(int x, int y) {
+    int a = find(x);
+    int b = find(y);
+    if(a > b) arr[b] = a;
+    else arr[a] = b;
+
+    return;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) arr[i] = i;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(arr2[i][j]) merge(i, j);
+        }
+    }
+
+    for(int i = 1; i <= n; i++) cout << arr[i] << ' ';
+    cout << '\n';
+
+    for(int i = 0; i < m; i++) cin >> arr[i];
+    for(int i = 1; i < m; i++) {
+        int a, b;
+        a = arr[i - 1];
+        b = arr[i];
+        cout << find(a) << " " << find(b) << "\n";
+        if(find(a) != find(b)) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+}
