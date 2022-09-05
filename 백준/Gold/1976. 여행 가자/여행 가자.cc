ca4740eba6d44ff4cfe0052ac73bdbfdf5ade4c9
Laniakea <skysaver00@naver.com>
2022-09-05 23:35:53 +0900

    [Gold IV] Title: 여행 가자, Time: 4 ms, Memory: 1272 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+bool arr[201][201];
+int line[1001];
+int check[201];
+int n, m;
+queue<int> pairQue;
+
+void startBFS(int i) {
+    pairQue.push(i);
+
+    while(!pairQue.empty()) {
+        int val = pairQue.front();
+        pairQue.pop();
+        check[val] = true;
+
+        for(int k = 1; k <= n; k++) {
+            if(arr[val][k] == 0 || check[k] != 0) continue;
+            else {
+                pairQue.push(k);
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i = 0; i < m; i++) scanf("%d", &line[i]);
+    /*for(int i = 1; i <= n; i++) {
+        startBFS(i);
+        for(int j = 1; j <= n; j++) check[j] = 0;
+    }*/
+    startBFS(line[0]);
+    for(int i = 0; i < m; i++) {
+        if(check[line[i]] == 0) {
+            printf("NO\n");
+            return 0;
+        }
+    }
+    printf("YES\n");
+    return 0;
+}
