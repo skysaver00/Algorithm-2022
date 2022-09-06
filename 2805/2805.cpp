fe9906faaf869e467ad4eac475f7e2310922c6de
skysaver00 <skysaver00@naver.com>
2022-09-07 01:27:16 +0900

    Not Solved 2022-09-07

---
+++ 2805/2805.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int arr[1000001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+
+    int low, high, mid;
+    low = 2000000001;
+    high = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(low > arr[i]) low = arr[i];
+        if(high < arr[i]) high = arr[i];
+    }
+
+    while(low + 1 < high) {
+        mid = (low + high) / 2;
+        long long sum = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > mid) sum += (arr[i] - mid);
+        }
+        if(sum >= m) low = mid;
+        else high = mid;
+    }
+    cout << low << '\n';
+}
