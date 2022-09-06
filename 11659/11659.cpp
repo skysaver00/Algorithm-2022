73ac124612ba7837441b203d9c2b8ae6f4693f18
skysaver00 <skysaver00@naver.com>
2022-09-06 21:12:34 +0900

    Solved Silver III 2022-09-06

--- 11659/11659.cpp
+++ 11659/11659.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int arr[100001];
 
 int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
     int n, m;
     cin >> n >> m;
     int val;
