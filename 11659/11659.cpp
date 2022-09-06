dabdedf18bbfeeb5e5226c3f5b172d82ad7d63f1
skysaver00 <skysaver00@naver.com>
2022-09-06 21:12:11 +0900

    Solved Silver III 2022-09-06

---
+++ 11659/11659.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int arr[100001];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int val;
+    cin >> val;
+    arr[1] = val;
+    for(int i = 2; i <= n; i++) {
+        int t;
+        cin >> t;
+        arr[i] = arr[i - 1] + t;
+    }
+
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << arr[b] - arr[a - 1] << '\n';
+    }
+}
