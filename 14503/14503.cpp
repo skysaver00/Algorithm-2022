a862cf5d5009b1b05fb18b48451c20bdae298ae6
skysaver00 <skysaver00@naver.com>
2022-09-06 23:22:32 +0900

    Solved Gold V 2022-09-06

--- 14503/14503.cpp
+++ 14503/14503.cpp
@@ -1,55 +1,40 @@
 #include <iostream>
 
 using namespace std;
+int n, m;
 int r, c, d;
 int room[52][52];
 int ans = 0;
 int Axisx[4] = {-1, 0, 1, 0};
-int Axisy[4] = {0, -1, 0, 1};
-bool flag = false;
+int Axisy[4] = {0, 1, 0, -1};
 
-int change(int x) {
-    x += 1;
-    if(x % 4 == 0) x = 0;
-    return x;
-}
-
-void startclean(int x, int y) {
+void startclean(int x, int y, int z) {
     if(room[x][y] == 0) {
         room[x][y] = 2;
         ans++;
-        cout << ans << '\n';
     }
     int ne, newx, newy;
 
-    while(1) {
-        for(int i = 0; i < 4; i++) {
-            d = change(d);
-            ne = d;
-            newx = x + Axisx[ne];
-            newy = y + Axisy[ne];
-            if(room[newx][newy] == 2 || room[newx][newy] == 1) continue;
-            cout << x << ' ' << Axisx[ne] << ' ' << y << ' ' << Axisy[ne] << '\n';
-
-            cout << newx << ' ' << newy << ' ' << room[newx][newy] << '\n';
-            if(room[newx][newy] == 0) startclean(newx, newy);
-        }
-
-        if(room[x + Axisx[(ne + 2) % 4]][y + Axisy[(ne + 2) % 4]] == 1) {
-            flag = true;
-            break;
-        }
-
-        x = x + Axisx[(ne + 2) % 4];
-        y = y + Axisy[(ne + 2) % 4];
+    for(int i = 0; i < 4; i++) {
+        ne = (z + 3 - i) % 4;
+        newx = x + Axisx[ne];
+        newy = y + Axisy[ne];
+        if(newx < 0 || newy < 0 || newx >= n || newy >= m) continue;
+        if(room[newx][newy] == 0) startclean(newx, newy, ne);
     }
 
-    return;
+    newx = x + Axisx[(z + 2) % 4];
+    newy = y + Axisy[(z + 2) % 4];
+
+    if(room[newx][newy] == 1) {
+        cout << ans << '\n';
+        exit(0);
+    }
+    startclean(newx, newy, z);
 }
 
 
 int main() {
-    int n, m;
     cin >> n >> m;
     cin >> r >> c >> d;
 
@@ -59,6 +44,5 @@ int main() {
         }
     }
 
-    startclean(r, c);
-    cout << ans << '\n';
+    startclean(r, c, d);
 }
