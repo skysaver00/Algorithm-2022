fd0c72b598628b61154a8c586ea30bbc7439ca53
skysaver00 <skysaver00@naver.com>
2022-09-07 23:13:08 +0900

    Not Solved 2022-09-07

--- 14499/14499.cpp
+++ 14499/14499.cpp
@@ -18,87 +18,81 @@ int main() {
     nowy = y;
     int up, down;
     up = 0, down = 2;
-    int l, r;
-    l = 0, r = 2;
     bool flag = 0;
     bool flag2 = 0;
     for(int i = 0; i < k; i++) {
         int order;
         cin >> order;
-        if(order == 3 || order == 4) {
-            if(order == 4) {
-                if(nowx + 1 >= n) continue;
-                if(r % 2 == 1)
-                up = (up + 1) % 4;
-                down = (down + 1) % 4;
-                nowx += 1;
-                dice[down][1] += room[nowx][nowy];
-                cout << dice[up][1] << '\n';
-            } else if(order == 3) {
-                if(nowx - 1 < 0) continue;
-                up = (up - 1) % 4;
-                down = (down - 1) % 4;
-                nowx -= 1;
-                dice[down][1] += room[nowx][nowy];
-                cout << dice[up][1] << '\n';
-            }
-        } else if(order == 1 || order == 2) {
-            if(order == 1) if(nowy + 1 >= m) continue;
-            if(order == 2) if(nowy - 1 < 0) continue;
+        if(order == 1) if(nowy + 1 >= m) continue;
+        if(order == 2) if(nowy - 1 < 0) continue;
+        if(order == 3) if(nowx - 1 < 0) continue;
+        if(order == 4) if(nowx + 1 >= n) continue;
+
+        if(order == 1 || order == 2) {
             if(order == 1) {
-                l = (l + 1) % 4;
-                r = (r + 1) % 4;
                 nowy += 1;
-                if(r % 2 == 1) {
-                    if(r == 3) {
-                        dice[0][2] += room[nowx][nowy];
-                        cout << dice[0][0] << '\n';
-                    } else if(r == 1) {
-                        dice[0][0] += room[nowx][nowy];
-                        cout << dice[0][2] << '\n';
-                    }
-                } else {
-                    if(r == 2) {
-                        dice[1][up] += room[nowx][nowy];
-                        cout << dice[1][down] << '\n';
-                    }
-                    if(r == 0) {
-                        dice[1][down] += room[nowx][nowy];
-                        cout << dice[1][up] << '\n';
-                    }
+                int temp = dice[0][0];
+                dice[0][0] = dice[0][1];
+                dice[0][1] = dice[0][2];
+                dice[0][2] = dice[2][1];
+                dice[2][1] = temp;
+                if(room[nowx][nowy] != 0) {
+                    dice[0][1] = room[nowx][nowy];
+                    room[nowx][nowy] = 0;
+                } if(room[nowx][nowy] == 0) {
+                    room[nowx][nowy] = dice[0][1];
                 }
-            }
-            if(order == 2) {
-                l = (l - 1) % 4;
-                r = (r - 1) % 4;
+            } else {
                 nowy -= 1;
-                if(r % 2 == 1) {
-                    if(r == 3) {
-                        dice[0][2] += room[nowx][nowy];
-                        cout << dice[0][0] << '\n';
-                    } else if(r == 1) {
-                        dice[0][0] += room[nowx][nowy];
-                        cout << dice[0][2] << '\n';
-                    }
-                } else {
-                    if(r == 2) {
-                        dice[1][up] += room[nowx][nowy];
-                        cout << dice[1][down] << '\n';
-                    }
-                    if(r == 0) {
-                        dice[1][down] += room[nowx][nowy];
-                        cout << dice[1][up] << '\n';
-                    }
+                int temp = dice[0][0];
+                dice[0][0] = dice[2][1];
+                dice[2][1] = dice[0][2];
+                dice[0][2] = dice[0][1];
+                dice[0][1] = temp;
+                if(room[nowx][nowy] != 0) {
+                    dice[0][1] = room[nowx][nowy];
+                    room[nowx][nowy] = 0;
+                } if(room[nowx][nowy] == 0) {
+                    room[nowx][nowy] = dice[0][1];
                 }
             }
+            cout << dice[2][1] << '\n';
+        } else if(order == 3 || order == 4) {
+            if(order == 3) {
+                nowx -= 1;
+                int temp = dice[0][1];
+                dice[0][1] = dice[1][1];
+                dice[1][1] = dice[2][1];
+                dice[2][1] = dice[3][1];
+                dice[3][1] = temp;
+                if(room[nowx][nowy] != 0) {
+                    dice[0][1] = room[nowx][nowy];
+                    room[nowx][nowy] = 0;
+                } if(room[nowx][nowy] == 0) {
+                    room[nowx][nowy] = dice[0][1];
+                }
+            } else {
+                nowx += 1;
+                int temp = dice[0][1];
+                dice[0][1] = dice[3][1];
+                dice[3][1] = dice[2][1];
+                dice[2][1] = dice[1][1];
+                dice[1][1] = temp;
+                if(room[nowx][nowy] != 0) {
+                    dice[0][1] = room[nowx][nowy];
+                    room[nowx][nowy] = 0;
+                } if(room[nowx][nowy] == 0) {
+                    room[nowx][nowy] = dice[0][1];
+                }
+            }
+            cout << dice[2][1] << '\n';
         }
-        for(int a = 0; a < 4; a++) {
+
+        /*for(int a = 0; a < 4; a++) {
             for(int b = 0; b < 3; b++) {
                 cout << dice[a][b] << ' ';
             }cout << '\n';
-        }
+        }*/
     }
-
-
     return 0;
 }
