846d7f7a422f808456802d2059a7fd405edc9aed
skysaver00 <skysaver00@naver.com>
2022-09-08 21:26:31 +0900

    Not Solved 2022-09-08

--- 15685/15685.cpp
+++ 15685/15685.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 
 using namespace std;
-queue <int> dragon;
+stack <int> dragon;
 queue <int> que;
 stack <int> stk;
 int arr[105][105];
@@ -17,14 +17,36 @@ int main() {
         int x, y, d, g;
         cin >> y >> x >> d >> g;
         int newx, newy;
-        newx = x;
-        newy = y;
+        arr[x][y] = 1;
+        cout << x << ' ' << y << '\n';
+        newx = x + axisx[d];
+        newy = y + axisy[d];
+        cout << newx << ' ' << newy << '\n';
         if(newx >= 0 && newx <= 100 && newy >= 0 && newy <= 100) arr[newx][newy] = 1;
         dragon.push(d);
-        for(int j = 0; j < g; j++) {
-            int top = dragon.top();
-            dragon.pop();
-            
+        for(int j = 1; j <= g; j++) {
+            while(!dragon.empty()) {
+                int top = dragon.top();
+                dragon.pop();
+                int newp = (top + 1) % 4;
+                newx = newx + axisx[newp];
+                newy = newy + axisy[newp];
+                cout << newx << ' ' << newy << '\n';
+                if(newx >= 0 && newx <= 100 && newy >= 0 && newy <= 100) arr[newx][newy] = 1;
+                stk.push(top);
+                que.push(newp);
+            }
+            while(!que.empty()) {
+                int front = que.front();
+                que.pop();
+                dragon.push(front);
+            }
+            while(!stk.empty()) {
+                int top = stk.top();
+                stk.pop();
+                dragon.push(top);
+            }
+            cout << '\n';
         }
     }
 }
