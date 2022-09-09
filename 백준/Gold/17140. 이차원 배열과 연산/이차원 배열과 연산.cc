d5bce983153f74844dfb88bd95b57f8e66448a4c
Laniakea <skysaver00@naver.com>
2022-09-09 14:44:52 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector <pair<int, int>> vec;
+int arr[105][105];
+
+int main() {
+    int r, c, k;
+    cin >> r >> c >> k;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int down, right;
+    down = right = 3;
+    int ans = 0;
+    while(1) {
+        if(ans > 100) {
+            cout << -1;
+            return 0;
+        }
+        if(arr[r - 1][c - 1] == k) {
+            cout << ans;
+            return 0;
+        }
+
+        if(right <= down) {
+            int tempright = 0;
+            for(int i = 0; i < down; i++) {
+                for(int j = 0; j < right; j++) {
+                    bool flag = false;
+                    int sz = vec.size();
+                    for(int k = 0; k < sz; k++) {
+                        if(vec[k].second == arr[i][j]) {
+                            flag = true;
+                            vec[k].first++;
+                            break;
+                        }
+                    }
+                    if(!flag) {
+                        if(arr[i][j] == 0) continue;
+                        vec.push_back(make_pair(1, arr[i][j]));
+                    }
+                }
+                sort(vec.begin(), vec.end());
+                int sz = vec.size();
+                int t = 0;
+                for(int j = 0; j < right; j++) arr[i][j] = 0;
+                for(int j = 0; j < sz; j++) {
+                    if(t == 100) break;
+                    arr[i][t] = vec[j].second;
+                    t++;
+                    arr[i][t] = vec[j].first;
+                    t++;
+                }
+                if(t > tempright) tempright = t;
+                vec.clear();
+            }
+            if(right < tempright) right = tempright;
+        } else {
+            int tempdown = 0;
+            for(int i = 0; i < right; i++) {
+                for(int j = 0; j < down; j++) {
+                    bool flag = false;
+                    int sz = vec.size();
+                    for(int k = 0; k < sz; k++) {
+                        if(vec[k].second == arr[j][i]) {
+                            flag = true;
+                            vec[k].first++;
+                            break;
+                        }
+                    }   
+                    if(!flag) {
+                        if(arr[j][i] == 0) continue;
+                        vec.push_back(make_pair(1, arr[j][i]));
+                    }
+                }
+                sort(vec.begin(), vec.end());
+                int sz = vec.size();
+                int t = 0;
+                for(int j = 0; j < down; j++) arr[j][i] = 0;
+                for(int j = 0; j < sz; j++) {
+                    if(t == 100) break;
+                    arr[t][i] = vec[j].second;
+                    t++;
+                    arr[t][i] = vec[j].first;
+                    t++;
+                }
+                if(t > tempdown) tempdown = t;
+                vec.clear();
+            }
+            if(down < tempdown) down = tempdown;
+        }
+        ans++;
+    }
+}
