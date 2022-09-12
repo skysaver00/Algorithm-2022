3ddd1bb39bc0a25f6e9a32f7bae9c1d6a54a8acb
Laniakea <skysaver00@naver.com>
2022-09-12 22:37:23 +0900

    [Silver I] Title: 경로 찾기, Time: 144 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int arr[101][101];
+bool check[101];
+int n;
+queue <int> que;
+
+bool startbfs(int i, int j) {
+    que.push(i);
+
+    while(!que.empty()) {
+        int front = que.front();
+        if(front == j && check[j]) {
+            while(!que.empty()) que.pop();
+            return true;
+        }
+        que.pop();
+        for(int i = 0; i < n; i++) {
+            if(arr[front][i] == 1 && !check[i]) {
+                check[i] = true;
+                que.push(i);
+            }  
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << startbfs(i, j) << ' ';
+            fill_n(check, 100, false);
+        }cout << '\n';
+    }
+}
