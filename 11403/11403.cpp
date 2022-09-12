12e67385966b413b3c67fd00bd67b87838177cfb
skysaver00 <skysaver00@naver.com>
2022-09-12 22:39:07 +0900

    Solved Silver I 2022-09-12

--- 11403/11403.cpp
+++ 11403/11403.cpp
@@ -28,6 +28,10 @@ bool startbfs(int i, int j) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+    
     cin >> n;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
