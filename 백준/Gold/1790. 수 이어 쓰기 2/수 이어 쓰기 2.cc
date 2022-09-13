a19b2182362d2588334eececd7fc74ae29007e24
Laniakea <skysaver00@naver.com>
2022-09-13 19:43:17 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+long long val;
+
+void getval(int i) {
+    long long now = 10;
+    long long j = 1;
+    long long k = 1;
+    while(now <= i) {
+        val += (j * 9) * k;
+        j *= 10;
+        k++;
+        now *= 10;
+    }
+    now /= 10;
+    val += (i - now + 1) * k;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    getval(n);
+
+    if(val < k) {
+        cout << "-1\n";
+        return 0;
+    }
+    val = 0;
+
+    int left = 1;
+    int right = n;
+    int ans, mid;
+    while(left <= right) {
+        mid = (left + right) / 2;
+        getval(mid);
+        if(val < k) left = mid + 1;
+        else {
+            ans = mid;
+            right = mid - 1;
+        }
+        val = 0;
+    }
+    getval(ans);
+    string s = to_string(ans);
+    long long l = val;
+    cout << s[s.length()-(l-k)-1] << '\n';
+}
