5f2ab1eafaf8e28e7fc7c7de79309bcec425edd5
skysaver00 <skysaver00@naver.com>
2022-09-14 19:47:23 +0900

    Solved Silver II 2022-09-14

--- 2003/2003.cpp
+++ 2003/2003.cpp
@@ -4,20 +4,27 @@ using namespace std;
 int a[10001];
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m;
     cin >> n >> m;
 
     for(int i = 0; i < n; i++) cin >> a[i];
 
     int ans = 0;
-    for(int i = 0; i < n; i++) {
-        int sum = 0;
-        for(int j = i; j < n; j++) {
-            sum += a[j];
-            if(sum == m) {
-                ans++;
-                break;
-            }
+    int sum = a[0];
+    int left, right;
+    left = 0; right = 0;
+    while(left < n && right < n) {
+        if(sum == m) ans++;
+        if(sum <= m) {
+            right++;
+            sum += a[right];
+        } else {
+            sum -= a[left];
+            left++;
         }
     }
 
