42ce2eb8c691339f3f77cb81b37fbe6b765e7452
skysaver00 <skysaver00@naver.com>
2022-09-15 02:18:11 +0900

    Not Solved 2022-09-15

--- 2531/2531.cpp
+++ 2531/2531.cpp
@@ -10,11 +10,41 @@ int main() {
 
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    for(int i = 0; i < k; i++) sushi[arr[i]]++;
+    bool flag = false;
+    bool flag2 = false;
+    int maxi, ans;
+    maxi = -1, ans = 0;
+    for(int i = 0; i < k; i++) {
+        if(sushi[arr[i]] == 0) flag = true;
+        sushi[arr[i]]++;
+        if(sushi[arr[i]] > 0 && flag) ans++;
+        flag = false;
+        if(maxi < ans) maxi = ans;
+    }
     int left, right;
     left = 0, right = k - 1;
 
     while(left / n == 0) {
-        
+        if(flag2) ans--;
+        flag2 = false;
+
+        sushi[arr[left % n]]--;
+        if(sushi[arr[left % n]] == 0) ans--;
+
+        left++;
+        right++;
+        sushi[arr[right % n]]++;
+
+        if(sushi[arr[right % n]] == 0) flag = true;
+        if(sushi[arr[right % n]] > 0 && flag) ans++;
+
+        flag = false;
+
+        if(sushi[arr[(left - 1) % n]] == 0 && arr[(left - 1) % n] == c) flag2 = true;
+        if(sushi[arr[(right + 1) % n]] == 0 && arr[(right + 1) % n] == c) flag2 = true;
+        if(flag2) ans++;
+        if(maxi < ans) maxi == ans;
+        cout << left << ' ' << right << '\n';
+        cout << maxi << '\n';
     }
 }
