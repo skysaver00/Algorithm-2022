eac8582b2112f18a017a68d863af3bc642057f20
skysaver00 <skysaver00@naver.com>
2022-09-15 23:29:33 +0900

    Solved Silver V 2022-09-15

--- 2018/2018.cpp
+++ 2018/2018.cpp
@@ -9,8 +9,20 @@ int main() {
     int left, right;
     left = 1;
     right = 1;
+    int sum = 1;
+    int ans = 0;
 
-    while(left < n) {
-        
+    while(left <= n) {
+        if(sum == n) ans++;
+
+        if(sum < n) {
+            right++;
+            sum += right;
+        } else {
+            sum -= left;
+            left++;
+        }
     }
+    cout << ans << '\n';
+    return 0;
 }
