83764b991f6baa675482b907e59860db84e1bbcd
skysaver00 <skysaver00@naver.com>
2022-09-15 23:25:12 +0900

    Not Solved 2022-09-15

---
+++ 2018/2018.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int left, right;
+    left = 1;
+    right = 1;
+
+    while(left < n) {
+        
+    }
+}
