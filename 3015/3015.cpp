c15b062b27dca72b0c08dafb299f3365be5cbc8e
skysaver00 <skysaver00@naver.com>
2022-09-15 23:00:37 +0900

    Not Solved 2022-09-15

---
+++ 3015/3015.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+int arr[500001];
+stack <int> stk;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+}
