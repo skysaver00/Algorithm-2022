be2207ba87b24b5841a3a3f67ac01443a9795463
Laniakea <skysaver00@naver.com>
2022-09-16 21:35:10 +0900

    [Gold V] Title: 용액, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+long long arr[100001];
+long long val = abs(2100000000);
+long long sum;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    int left, right;
+    left = 0; right = n - 1;
+    int lans, rans;
+    while(left != right) {
+        sum = abs(arr[left] + arr[right]);
+        if(sum <= val) {
+            val = sum;
+            lans = arr[left];
+            rans = arr[right];
+        }
+        if(arr[left] + arr[right] <= 0) left++;
+        else right--;
+    }
+
+    cout << lans << ' ' << rans << '\n';
+    return 0;
+}
