a607b424f2ae8eb738cb0e4dbb5671e637b33974
skysaver00 <skysaver00@naver.com>
2022-09-17 16:42:26 +0900

    2022-09-17

---
+++ 2473_2/2473_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+long long arr[5001];
+long long sum;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    
+    long long first, second, third;
+    for(int i = 0; i < n; i++) {
+        first = i;
+        second = i + 1;
+        third = n - 1;
+        while(second <= third) {
+            
+        }
+    }
+}
