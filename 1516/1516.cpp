0b1d51d5e661ad958b29918b7a4e76f70794d832
skysaver00 <skysaver00@naver.com>
2022-09-18 14:54:00 +0900

    Not Solved 2022-09-18

---
+++ 1516/1516.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+priority_queue <int, vector<int>, greater<int>> pq;
+vector <int> vec[501];
+int ind[501];
+
+int main() {
+    
+}
