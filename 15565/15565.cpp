369ff5a6c5afe711da2019ca2a2a178ba5197483
skysaver00 <skysaver00@naver.com>
2022-09-18 14:06:45 +0900

    Not Solved 2022-09-18

--- 15565/15565.cpp
+++ 15565/15565.cpp
@@ -14,21 +14,25 @@ int main() {
     for(int i = 0; i < n; i++) cin >> arr[i];
     int left, right;
     int ans, now;
+    now = 0;
     ans = 99999999;
     left = 0;
     right = 0;
     if(arr[left] == 1) now++;
 
     while(right < n) {
+        cout << left << ' ' << right << ' ' << now << '\n';
         if(now < k) right++;
         else {
             int sub = right - left;
             if(sub < ans) ans = sub;
             left++;
+            right++;
+            while(arr[left] != 1) left++;
             now--;
         }
 
         if(arr[right] == 1) now++;
     }
-    cout << ans << '\n';
+    cout << ans + 1 << '\n';
 }
