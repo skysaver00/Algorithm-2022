c5bdcc8bbdb1524d1ad26fdc3d254decf93761d7
skysaver00 <skysaver00@naver.com>
2022-09-25 01:01:30 +0900

    Not Solved 2022-09-25

--- 21610/21610.cpp
+++ 21610/21610.cpp
@@ -19,4 +19,95 @@ int main() {
     vec.push_back({n, 2});
     vec.push_back({n - 1, 1});
     vec.push_back({n - 1, 2});
+
+    for(int k = 0; k < m; k++) {
+        int d, s;
+        cin >> d >> s;
+
+        int sz = vec.size();
+        if(d == 1) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second - s <= 0) {
+                    vec[i].second += n;
+                    vec[i].second -= s;
+                } else vec[i].second -= s;
+            }
+        }
+        if(d == 2) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second - s <= 0) {
+                    vec[i].second += n;
+                    vec[i].second -= s;
+                } else vec[i].second -= s;
+                if(vec[i].first - s <= 0) {
+                    vec[i].first += n;
+                    vec[i].first -= s;
+                } else vec[i].first -= s;
+            }
+        }
+        if(d == 3) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].first - s <= 0) {
+                    vec[i].first += n;
+                    vec[i].first -= s;
+                } else vec[i].first -= s;
+            }
+        }
+        if(d == 4) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second + s > n) {
+                    vec[i].second -= n;
+                    vec[i].second += s;
+                } else vec[i].second += s;
+                if(vec[i].first - s <= 0) {
+                    vec[i].first += n;
+                    vec[i].first -= s;
+                } else vec[i].first -= s;
+            }
+        }
+        if(d == 5) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second + s > n) {
+                    vec[i].second -= n;
+                    vec[i].second += s;
+                } else vec[i].second += s;
+            }
+        }
+        if(d == 6) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second + s > n) {
+                    vec[i].second -= n;
+                    vec[i].second += s;
+                } else vec[i].second += s;
+                if(vec[i].first + s > n) {
+                    vec[i].first -= n;
+                    vec[i].first += s;
+                } else vec[i].first += s;
+            }
+        }
+        if(d == 7) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].first + s > n) {
+                    vec[i].first -= n;
+                    vec[i].first += s;
+                } else vec[i].first += s;
+            }
+        }
+        if(d == 8) {
+            for(int i = 0; i < sz; i++) {
+                if(vec[i].second - s <= 0) {
+                    vec[i].second += n;
+                    vec[i].second -= s;
+                } else vec[i].second -= s;
+                if(vec[i].first + s > n) {
+                    vec[i].first -= n;
+                    vec[i].first += s;
+                } else vec[i].first += s;
+            }
+        }
+
+        for(int i = 0; i < sz; i++) {
+            cout << vec[i].first << ' ' << vec[i].second << '\n';
+        }
+    }
 }
