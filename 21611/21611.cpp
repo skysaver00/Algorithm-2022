3ba693fde88123a9754228503b743a8f837e5c07
skysaver00 <skysaver00@naver.com>
2022-09-25 18:02:02 +0900

    Not Solved 2022-09-25

--- 21611/21611.cpp
+++ 21611/21611.cpp
@@ -14,10 +14,29 @@ int main() {
 
     int shx, shy;
     shx = shy = (n / 2) + 1;
-    
+
     for(int i = 0; i < m; i++) {
         int d, s;
         cin >> d >> s;
-
+        if(d == 1) {
+            for(int j = 1; j <= s; j++) {
+                if(shx - j >= 0) arr[shx - j][shy] = 0;
+            }
+        }
+        if(d == 2) {
+            for(int j = 1; j <= s; j++) {
+                if(shx + j <= n) arr[shx + j][shy] = 0;
+            }
+        }
+        if(d == 3) {
+            for(int j = 1; j <= s; j++) {
+                if(shy - j >= 0) arr[shx][shy - j] = 0;
+            }
+        }
+        if(d == 4) {
+            for(int j = 1; j <= s; j++) {
+                if(shy + j <= n) arr[shx][shy + j] = 0;
+            }
+        }
     }
 }
