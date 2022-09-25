96e3b4fe55e420b46326c0db9439b139a512dd86
skysaver00 <skysaver00@naver.com>
2022-09-25 17:54:50 +0900

    Not Solved 2022-09-25

--- 21611/21611.cpp
+++ 21611/21611.cpp
@@ -11,4 +11,13 @@ int main() {
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) cin >> arr[i][j];
     }
+
+    int shx, shy;
+    shx = shy = (n / 2) + 1;
+    
+    for(int i = 0; i < m; i++) {
+        int d, s;
+        cin >> d >> s;
+
+    }
 }
