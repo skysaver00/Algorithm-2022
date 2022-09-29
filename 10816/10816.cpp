e74c28c55e96f3e664cb6069462fa0846739508d
skysaver00 <skysaver00@naver.com>
2022-09-30 01:13:05 +0900

    Solved Silver IV 2022-09-30

--- 10816/10816.cpp
+++ 10816/10816.cpp
@@ -7,6 +7,10 @@ vector <pair<int, int>> vec;
 int arr[500001];
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) {
@@ -16,39 +20,45 @@ int main() {
     }
 
     sort(arr, arr + n);
-    
+
     int now = arr[0];
-    vec.push_back({now, 0});
+    int cntnow = 0;
+    vec.push_back({now, 1});
     for(int i = 1; i < n; i++) {
         if(now != arr[i]) {
             now = arr[i];
-            vec.push_back({now, 0});
+            cntnow++;
+            vec.push_back({now, 1});
+        } else {
+            vec[cntnow].second++;
         }
     }
 
     int left, mid, right;
-    for(int i = 0; i < n; i++) {
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        int l;
+        cin >> l;
         left = 0;
         right = vec.size();
-        int target = arr[i];
+        int target = l;
+        int flag = 0;
 
         while(left <= right) {
             mid = (left + right) / 2;
             if(vec[mid].first < target) {
-                right = mid - 1;
-            } else if(vec[mid].first > target) {
                 left = mid + 1;
+            } else if(vec[mid].first > target) {
+                right = mid - 1;
             } else {
+                flag = 1;
                 break;
             }
         }
 
-        vec[mid].second++;
-    }
-
-    int sz = vec.size();
-    for(int i = 0; i < sz; i++) {
-        cout << vec[i].first << ' ' << vec[i].second << '\n';
+        if(flag) cout << vec[mid].second << ' ';
+        else cout << "0 ";
     }
 
     return 0;
