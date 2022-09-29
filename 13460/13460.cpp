37d59bf257f2bd254096c5c7e2069ddd8b7eee37
skysaver00 <skysaver00@naver.com>
2022-09-30 01:48:53 +0900

    Not Solved 2022-09-30

--- 13460/13460.cpp
+++ 13460/13460.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-queue <pair<int, pair<int, int>>> que;
+queue <pair<int, pair<int, int>>> red;
+queue <pair<int, pair<int, int>>> blue;
 char arr[12][12];
+
+int x_[4] = {1, 0, -1, 0};
+int y_[4] = {0, 1, 0, -1};
 
 int main() {
     int n, m;
@@ -15,6 +19,45 @@ int main() {
             char c;
             cin >> c;
             arr[i][j] = c;
+            if(c == 'R') {
+                red.push({0, {i, j}});
+                arr[i][j] = '.';
+            }
+            if(c == 'B') {
+                blue.push({0, {i, j}});
+                arr[i][j] = '.';
+            }
+        }
+    }
+
+    int flag = 0;
+
+    while(!red.empty()) {
+        int redx, redy, blux, bluy;
+        int ind = red.front().first;
+        redx = red.front().second.first;
+        redy = red.front().second.second;
+        blux = blue.front().second.second;
+        bluy = blue.front().second.second;
+        red.pop();
+        blue.pop();
+
+        int rstop, bstop;
+        rstop = bstop = 0;
+        for(int i = 0; i < 4; i++) {
+            int nredx, nredy, nblux, nbluy;
+            nredx = redx;
+            nredy = redy;
+            nblux = blux;
+            nbluy = bluy;
+            while(rstop == 0 || bstop == 0) {
+                nredx += x_[i];
+                nredy += y_[i];
+                nblux += x_[i];
+                nbluy += y_[i];
+
+                
+            }
         }
     }
 
@@ -24,4 +67,5 @@ int main() {
         }cout << '\n';
     }
 
+    return 0;
 }
