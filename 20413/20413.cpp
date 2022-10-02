ca0b962a37c7816331f17102ccf4c88e3f161824
skysaver00 <skysaver00@naver.com>
2022-10-02 23:45:27 +0900

    Not Solved 2022-10-02

--- 20413/20413.cpp
+++ 20413/20413.cpp
@@ -3,9 +3,36 @@
 
 using namespace std;
 int s, g, p, d;
+int price[40];
 
 int main() {
     int n;
     cin >> n;
     cin >> s >> g >> p >> d;
+
+    string str;
+    cin >> str;
+
+    int len = str.length();
+    for(int i = 0; i < len; i++) {
+        char val = str[i];
+        if(val == 'B') {
+            price[i + 1] = (s - 1) - price[i];
+        } else if(val == 'S') {
+            price[i + 1] = (g - 1) - price[i];
+        } else if(val == 'G') {
+            price[i + 1] = (p - 1) - price[i];
+        } else if(val == 'P') {
+            price[i + 1] = (d - 1) - price[i];
+        } else {
+            price[i + 1] = 500 - price[i];
+        }
+    }
+    int ans = 0;
+    for(int i = 1; i <= len; i++) {
+        cout << price[i] << ' ';
+        ans += price[i];
+    }cout << '\n';
+    cout << ans;
+    return 0;
 }
