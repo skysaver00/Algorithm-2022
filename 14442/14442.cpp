1bd7124f6f7dddff81604ed399d147b0d2e808fc
skysaver00 <skysaver00@naver.com>
2022-10-04 21:08:24 +0900

    Not Solved 2022-10-04

--- 14442/14442.cpp
+++ 14442/14442.cpp
@@ -7,12 +7,14 @@ int n, m, k;
 queue <pair<pair<int, int>, int>> que;
 int mp[1001][1001];
 int val[1001][1001];
+bool ck[1001][1001];
 
 int x_[4] = {1, 0, -1, 0};
 int y_[4] = {0, 1, 0, -1};
 
 void startbfs(int i, int j, int k) {
     que.push({{i, j}, k});
+    ck[i][j] = true;
     val[i][j] = 1;
 
     while(!que.empty()) {
@@ -27,9 +29,13 @@ void startbfs(int i, int j, int k) {
             int newx, newy;
             newx = prex + x_[i];
             newy = prey + y_[i];
+            cout << newx << ' ' << newy << '\n';
 
-            if(newx < 0 || newx > n || newy < 0 || newy > m) continue;
+            if(newx < 0 || newx >= n || newy < 0 || newy >= m) continue;
             else {
+                if(ck[newx][newy]) continue;
+                ck[newx][newy] = true;
+
                 if(mp[newx][newy] == 1) {
                     if(trou + 1 <= k) {
                         que.push({{newx, newy}, trou + 1});
@@ -41,25 +47,22 @@ void startbfs(int i, int j, int k) {
                 }
             }
         }
-
-        for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << val[i][j] << ' ';
-        }cout << '\n';
     }
 
-    }
+    return;
 }
 
 int main() {
     cin >> n >> m >> k;
 
+    string str;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cin >> mp[i][j];
+        cin >> str;
+        int len = str.length();
+        for(int j = 0; j < len; j++) {
+            mp[i][j] = str[j] - 0x30;
         }
     }
-
     startbfs(0, 0, 0);
 
     for(int i = 0; i < n; i++) {
