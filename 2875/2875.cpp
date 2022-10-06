988869acd69630f3f2890570b7fb18ef31a72b4f
skysaver00 <skysaver00@naver.com>
2022-10-07 01:07:58 +0900

    Not Solved 2022-10-07

---
+++ 2875/2875.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int n, m, k;
+
+int main() {
+    cin >> n >> m >> k;
+    int ingman, ingwoman;
+    ingman = ingwoman = 0;
+
+    int nn = n / 2;
+    if(nn > m) {
+        ingwoman = n - m * 2;
+        n = m * 2;
+    } else if(nn < m) {
+        ingman = m - n / 2;
+        m = n / 2;
+    }
+
+    if(nn > m) {
+        ingwoman = n - m * 2;
+        n = m * 2;
+    }
+    
+
+    if(ingman + ingwoman >= k) cout << m;
+    else {
+        while(ingman + ingwoman < k) {
+            ingman += 1;
+            ingwoman += 2;
+            m--;
+        }
+        cout << m;
+    }
+}
