e2ffc1c746cd1ff22ad3b01228b95fd564c3cb31
skysaver00 <skysaver00@naver.com>
2022-10-09 22:17:01 +0900

    Solved Silver II 2022-10-09

--- 20006/20006.cpp
+++ 20006/20006.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
-vector < pair<int, string> > player;
-vector < pair<int, string> > room[301];
+vector < pair<string, int> > room[301];
 
 int main() {
     int p, m;
@@ -17,13 +16,38 @@ int main() {
         cin >> level >> name;
 
         if(now == -1) {
-            room[0].push_back({level, name});
+            room[0].push_back({name, level});
             now++;
         }
         else {
+            bool flag = false;
             for(int i = 0; i <= now; i++) {
-                if(level < room[i][0].first)
+                if(level <= room[i][0].second + 10 && level >= room[i][0].second - 10 && room[i].size() < m) {
+                    room[i].push_back({name, level});
+                    flag = true;
+                    break;
+                }
             }
+
+            if(!flag) {
+                room[now + 1].push_back({name, level});
+                now++;
+            }
+        }
+    }
+
+    for(int i = 0; i <= now; i++) {
+        sort(room[i].begin(), room[i].end());
+    }
+
+    for(int i = 0; i <= now; i++) {
+        int sz = room[i].size();
+        if(sz == 0) {}
+        else if(sz < m) cout << "Waiting!\n";
+        else cout << "Started!\n";
+
+        for(int j = 0; j < sz; j++) {
+            cout << room[i][j].second << ' ' << room[i][j].first << '\n';
         }
     }
 
