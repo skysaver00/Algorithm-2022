43318c6e8ae8b12a02f802b3039a36e9057d01d2
skysaver00 <skysaver00@naver.com>
2022-10-09 21:57:23 +0900

    Not Solved 2022-10-09

---
+++ 20006/20006.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector < pair<int, int> > player;
+vector < pair<int, int> > room[301];
+
+int main() {
+    int p, m;
+    cin >> p >> m;
+
+    for(int i = 0; i < )
+
+    return 0;
+}
