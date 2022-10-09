36dec1fb8994fe71c5854f91708d644caee26227
skysaver00 <skysaver00@naver.com>
2022-10-09 22:04:13 +0900

    Not Solved 2022-10-09

--- 20006/20006.cpp
+++ 20006/20006.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
-vector < pair<int, int> > player;
-vector < pair<int, int> > room[301];
+vector < pair<int, string> > player;
+vector < pair<int, string> > room[301];
 
 int main() {
     int p, m;
     cin >> p >> m;
 
-    for(int i = 0; i < )
+    int now = -1;
+    for(int i = 0; i < p; i++) {
+        int level;
+        string name;
+        cin >> level >> name;
+
+        if(now == -1) {
+            room[0].push_back({level, name});
+            now++;
+        }
+        else {
+            for(int i = 0; i <= now; i++) {
+                if(level < room[i][0].first)
+            }
+        }
+    }
 
     return 0;
 }
