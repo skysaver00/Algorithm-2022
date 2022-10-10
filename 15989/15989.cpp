84713be4ee5d46960e3b234bd11b12458f82024d
skysaver00 <skysaver00@naver.com>
2022-10-10 22:13:13 +0900

    Solved Silver I 2022-10-10

--- 15989/15989.cpp
+++ 15989/15989.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-int ans[10001][3];
+long long ans[10001][3];
 
 int main() {
     ans[1][1] = 1;
@@ -9,22 +9,25 @@ int main() {
     ans[3][3] = 1;
 
     int n;
+    int maxi = -1;
     cin >> n;
     for(int j = 0; j < n; j++) {
         int t;
         cin >> t;
         
+        if(maxi >= t) {
+            cout << ans[t][1] + ans[t][2] + ans[t][3] << '\n';
+            continue;
+        }
+        if(maxi < t) maxi = t;
+        
         for(int i = 2; i <= t; i++) {
-            ans[i][1] = ((ans[i - 1][1] + ans[i - 1][2] + ans[i - 1][3]) % 10000);
-            if(i > 2) ans[i][2] = ((ans[i - 2][1] + ans[i - 2][2] + ans[i - 2][3]) % 10000);
-            if(i > 3) ans[i][3] = ((ans[i - 3][1] + ans[i - 3][2] + ans[i - 3][3]) % 10000);
+            ans[i][1] = (ans[i - 1][1]);
+            if(i > 2) ans[i][2] = (ans[i - 2][1] + ans[i - 2][2]);
+            if(i > 3) ans[i][3] = (ans[i - 3][1] + ans[i - 3][2] + ans[i - 3][3]);
         }
-
-        for(int i = 1; i <= t; i++) {
-            for(int k = 1; k <= 3; k++) {
-                cout << ans[i][k] << ' ';
-            }cout << '\n';
-        }
+        
+        cout << ans[t][1] + ans[t][2] + ans[t][3] << '\n';
     }
     return 0;
 }
