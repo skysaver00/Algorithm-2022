7966f6955de640cc3f4093fafe19c9617bd90c4a
skysaver00 <skysaver00@naver.com>
2022-10-12 16:01:37 +0900

    Not Solved 2022-10-12

---
+++ 23288/23288.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int arr[21][21];
+int sum[21][21];
+
+queue < pair<int, int> > que;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for(int i = 0; i < n; i++) {
+        for(int i = 0; i < k; i++) {
+            cin >> arr[i][k];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int i = 0; i < k; i++) {
+        }
+    }
+
+    return 0;
+}
